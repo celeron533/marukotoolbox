2dd46bd96a041f5bd8c9e5d0f9349627c8271fa0
celeron533 <zhuyilun8@hotmail.com>
2016-02-21 10:29:13 +0800

    Init new project mp4box2

---
+++ mp4box2/Configuration/PersistableObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace mp4box2.Configuration
+{
+    public class PersistableObject
+    {
+        public static T Load<T>(string fileName) where T : PersistableObject, new()
+        {
+            T result = default(T);
+
+            using (XmlReader xmlReader = XmlReader.Create(fileName))
+            {
+                result = new XmlSerializer(typeof(T)).Deserialize(xmlReader) as T;
+            }
+            return result;
+        }
+
+
+
+        XmlWriterSettings xmlWriterSettings = new XmlWriterSettings
+        {
+            Indent = true,
+            OmitXmlDeclaration = false,
+            Encoding = Encoding.UTF8
+        };
+
+        public void Save<T>(string fileName) where T : PersistableObject
+        {
+            using (XmlWriter xmlWriter = XmlWriter.Create(fileName, xmlWriterSettings))
+            {
+                new XmlSerializer(typeof(T)).Serialize(xmlWriter, this);
+            }
+        }
+    }
+}
---
+++ mp4box2/Configuration/Preset.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace mp4box2.Configuration
+{
+
+    [Serializable]
+    [XmlRoot("Preset")]
+    public partial class Preset : PersistableObject
+    {
+        [XmlElement("Video")]
+        public Video video = new Video();
+
+        [XmlElement("Audio")]
+        public Audio audio = new Audio();
+    }
+
+    //Preset/Video
+    public class Video
+    {
+        [XmlElement("VideoEncoder")]
+        public VideoEncoder videoEncoder = new VideoEncoder();
+    }
+
+    //Preset/Audio
+    public class Audio
+    {
+        [XmlElement("AudioEncoder")]
+        public AudioEncoder audioEncoder = new AudioEncoder();
+    }
+
+
+    //Preset/Video/VideoEncoder
+    public class VideoEncoder
+    {
+        [XmlArray("X264")]
+        [XmlArrayItem("Parameter")]
+        public List<Parameter> x264 = new List<Parameter>();
+
+        [XmlArray("X265")]
+        [XmlArrayItem("Parameter")]
+        public List<Parameter> x265 = new List<Parameter>();
+    }
+
+
+    //Preset/Audio/AudioEncoder
+    public class AudioEncoder
+    {
+        [XmlArray("NeroAAC")]
+        [XmlArrayItem("Parameter")]
+        public List<Parameter> NeroAAC = new List<Parameter>();
+
+        [XmlArray("FDKAAC")]
+        [XmlArrayItem("Parameter")]
+        public List<Parameter> FDKAAC = new List<Parameter>();
+
+        [XmlArray("QAAC")]
+        [XmlArrayItem("Parameter")]
+        public List<Parameter> QAAC = new List<Parameter>();
+    }
+
+
+
+    public class Parameter
+    {
+        [XmlAttribute("Name")]
+        public string name;
+
+        [XmlText]
+        public string value;
+
+        public override string ToString()
+        {
+            return string.Format("name: {0}", name);
+        }
+    }
+
+
+}
