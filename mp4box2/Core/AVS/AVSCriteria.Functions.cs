2e87c5a9d850e592990648a866870583cfa96228
celeron533 <zhuyilun8@hotmail.com>
2016-02-25 17:07:49 +0800

    Add AVSCriteria

---
+++ mp4box2/Core/AVS/AVSCriteria.Functions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mp4box2.Core.AVS
+{
+    //Base
+    public class EnableStatus
+    {
+        public bool enabled;
+    }
+
+    public class UnDot : EnableStatus
+    {
+        public bool unDot;
+    }
+
+    //http://avisynth.nl/index.php/Tweak
+    public class Tweak : EnableStatus
+    {
+        public float chroma;
+        public float saturation;
+        public float brightness;
+        public float contrast;
+    }
+
+    //http://avisynth.nl/index.php/LanczosResize
+    public class LanczosResize : EnableStatus
+    {
+        public int width;
+        public int height;
+    }
+
+    //http://avisynth.nl/index.php/Levels
+    public class Levels : EnableStatus
+    {
+        public int inputLow;
+        public float gamma;
+        public int inputHigh;
+        public int outputLow;
+        public int outputHigh;
+    }
+
+    //http://avisynth.nl/index.php/Sharpen
+    public class Sharpen : EnableStatus
+    {
+        public bool sharpen;
+    }
+
+    //http://avisynth.nl/index.php/Crop
+    public class Crop : EnableStatus
+    {
+        public int left;
+        public int top;
+        public int width;  //right if <0
+        public int height; //bottom if <0
+    }
+
+    public class AddBorders : EnableStatus
+    {
+        public int left;
+        public int top;
+        public int right;
+        public int bottom;
+    }
+
+    //http://avisynth.nl/index.php/Trim
+    public class Trim:EnableStatus
+    {
+        public int startFrame;
+        public int endFrame;
+    }
+}
---
+++ mp4box2/Core/AVS/AVSCriteria.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mp4box2.Core.AVS
+{
+    public class AVSCriteria
+    {
+        #region Dll files
+        public string VSFilterDll;
+        public string SupTitleDll;
+        public string LSMASHSourceDll;
+        public string UnDotDll;
+        #endregion
+
+        public string inputVideoFile;
+        public string inputSubtitleFile;
+        public string outputVideoFile;
+
+
+        public UnDot unDot;
+
+        public Tweak tweak;
+
+        public LanczosResize lanczosResize;
+
+        public Sharpen sharpen;
+
+        public Levels levels;
+
+        public Crop crop;
+
+        public Trim trim;
+
+    }
+
+    
+}
