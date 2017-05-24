d96a596a72ff99db4adc70484dc2c605176d1619
celeron533 <zhuyilun8@hotmail.com>
2017-05-24 13:54:55 +0800

    Separate "preset" to new class.

--- mp4box/MainForm.cs
+++ mp4box/MainForm.cs
@@ -49,7 +49,7 @@ namespace mp4box
         public string workPath = "!undefined";
         public bool shutdownState = false;
         public bool trayMode = false;
-        private XDocument xdoc;
+        private Preset preset;
 
         #region Private Members Declaration
 
@@ -104,6 +104,7 @@ namespace mp4box
             Instance = this;
             logPath = Application.StartupPath + "\\logs";
             logFileName = logPath + "\\LogFile-" + DateTime.Now.ToString("yyyy'-'MM'-'dd'_'HH'-'mm'-'ss") + ".log";
+            preset = new Preset();
             InitializeComponent();
         }
 
@@ -1077,82 +1078,11 @@ namespace mp4box
 
         #endregion
 
-        private void PresetXml()
-        {
-            if (!File.Exists("preset.xml"))
-            {
-                xdoc = new XDocument(
-                    new XDeclaration("1.0", "utf-8", "yes"),
-                    new XElement("root",
-                        new XElement("Video",
-                            new XElement("VideoEncoder",
-                                new XElement("x264",
-                                    new XElement("Parameter", "--crf 24 --preset 8 -r 6 -b 6 -i 1 --scenecut 60 -f 1:1 --qcomp 0.5 --psy-rd 0.3:0 --aq-mode 2 --aq-strength 0.8 --vf resize:960,540,,,,lanczos", new XAttribute("Name", "default")),
-                                    new XElement("Parameter", "-p1 --crf 20 --aq-mode 2 --sar 32:27 --vf yadif:, --stat \"TEMP.stat\" --slow-firstpass --ref 8 --preset 8 --subme 10", new XAttribute("Name", "DVDRIP不切边,16:9")),
-                                    new XElement("Parameter", "-p1 --crf 20 --aq-mode 2 --sar 40:33 --vf yadif:, --stat \"TEMP.stat\" --slow-firstpass --ref 8 --preset 8 --subme 10", new XAttribute("Name", "DVDRIP不切边,sar40:33")),
-                                    new XElement("Parameter", "-p1 --crf 20 --aq-mode 2 --sar 40:33 --vf yadif:,/crop:8,0,8,0 --stat \"TEMP.stat\" --slow-firstpass --ref 8 --preset 8 --subme 10", new XAttribute("Name", "DVDRIP切边,sar40:33")),
-                                    new XElement("Parameter", "--profile high --level 3.1 --level-force  --device iphone", new XAttribute("Name", "iOS")),
-                                    new XElement("Parameter", "--crf 25 --preset placebo --subme 10 --ref 7 --bframes 7 --qcomp 0.75 --psy-rd 0:0 --keyint infinite --min-keyint 1", new XAttribute("Name", "MAD")),
-                                    new XElement("Parameter", "--profile main --level 3.0 --ref 3 --b-pyramid none --weightp 1 --vbv-maxrate 10000 --vbv-bufsize 10000   --vf resize:480,272,,,,lanczos  --device psp", new XAttribute("Name", "PSP"))
-                                    ),
-                                new XElement("x265",
-                                    new XElement("Parameter", "", new XAttribute("Name", "x265default"))
-                                    )
-                                )
-                            ),
-                        new XElement("Audio",
-                            new XElement("AudioEncoder",
-                                new XElement("NeroAAC",
-                                    new XElement("Parameter", "-he -br 32000", new XAttribute("Name", "NeroAAC_HE-32Kbps")),
-                                    new XElement("Parameter", "-he -br 48000", new XAttribute("Name", "NeroAAC_HE-48Kbps")),
-                                    new XElement("Parameter", "-he -br 64000", new XAttribute("Name", "NeroAAC_HE-64Kbps")),
-                                    new XElement("Parameter", "-lc -br 128000", new XAttribute("Name", "NeroAAC_LC-128Kbps")),
-                                    new XElement("Parameter", "-lc -br 192000", new XAttribute("Name", "NeroAAC_LC-192Kbps")),
-                                    new XElement("Parameter", "-lc -br 256000", new XAttribute("Name", "NeroAAC_LC-256Kbps")),
-                                    new XElement("Parameter", "-q 1 -lc", new XAttribute("Name", "NeroAAC_LC-Q1"))
-                                    ),
-                                new XElement("FDKAAC",
-                                     new XElement("Parameter", "-m 1", new XAttribute("Name", "FDKAAC_VBR1")),
-                                     new XElement("Parameter", "-m 2", new XAttribute("Name", "FDKAAC_VBR2")),
-                                     new XElement("Parameter", "-m 3", new XAttribute("Name", "FDKAAC_VBR3")),
-                                     new XElement("Parameter", "-m 4", new XAttribute("Name", "FDKAAC_VBR4")),
-                                     new XElement("Parameter", "-m 5", new XAttribute("Name", "FDKAAC_VBR5")),
-                                     new XElement("Parameter", "-p 5 -b 64", new XAttribute("Name", "FDKAAC_HE-_CBR64Kbps")),
-                                     new XElement("Parameter", "-b 128", new XAttribute("Name", "FDKAAC_LC-_CBR128Kbps")),
-                                     new XElement("Parameter", "-b 192", new XAttribute("Name", "FDKAAC_LC-_CBR192Kbps")),
-                                     new XElement("Parameter", "-b 256", new XAttribute("Name", "FDKAAC_LC-_CBR256Kbps"))
-                                     ),
-                                new XElement("QAAC",
-                                    new XElement("Parameter", "--he -c 64 -q 2 --no-optimize", new XAttribute("Name", "QAAC_HE-CBR64Kbps")),
-                                    new XElement("Parameter", "--he -v 64 -q 2 --no-optimize", new XAttribute("Name", "QAAC_HE-CVBR64Kbps")),
-                                    new XElement("Parameter", "-c 128 -q 2 --no-optimize", new XAttribute("Name", "QAAC_LC-CBR128Kbps")),
-                                    new XElement("Parameter", "-v 128 -q 2 --no-optimize", new XAttribute("Name", "QAAC_LC-CVBR128Kbps")),
-                                    new XElement("Parameter", "-c 256 -q 2 --no-optimize", new XAttribute("Name", "QAAC_LC-CBR256Kbps")),
-                                    new XElement("Parameter", "-v 256 -q 2 --no-optimize", new XAttribute("Name", "QAAC_LC-CVBR256Kbps")),
-                                    new XElement("Parameter", "-V 90 -q 2 --no-optimize", new XAttribute("Name", "QAAC_TVBR_V90")),
-                                    new XElement("Parameter", "-V 127 -q 2 --no-optimize", new XAttribute("Name", "QAAC_TVBR_V127"))
-                                    ),
-                                new XElement("MP3",
-                                    new XElement("Parameter", "--alt-preset extreme", new XAttribute("Name", "extreme")),
-                                    new XElement("Parameter", "--preset insane", new XAttribute("Name", "insane")),
-                                    new XElement("Parameter", "--preset standard", new XAttribute("Name", "standard")),
-                                    new XElement("Parameter", "--preset medium ", new XAttribute("Name", "medium"))
-                                    )
-                                )
-                            )
-                        )
-                );
-                xdoc.Save("preset.xml");
-            }
-            else
-                xdoc = XDocument.Load("preset.xml");
-        }
-
         private void LoadVideoPreset()
         {
             VideoPresetComboBox.Items.Clear();
             string encType = x264ExeComboBox.SelectedItem.ToString().Contains("x265") ? "x265" : "x264";
-            var xlsv = xdoc.Element("root").Element("Video").Element("VideoEncoder").Element(encType).Elements();
+            var xlsv = preset.GetVideoPreset(encType).Elements();
             foreach (var item in xlsv)
             {
                 VideoPresetComboBox.Items.Add(item.Attribute("Name").Value);
@@ -1164,7 +1094,7 @@ namespace mp4box
         private void LoadAudioPreset()
         {
             AudioPresetComboBox.Items.Clear();
-            var xlsa = xdoc.Element("root").Element("Audio").Element("AudioEncoder").Element(AudioEncoderComboBox.Text).Elements();
+            var xlsa = preset.GetAudioPreset(AudioEncoderComboBox.Text).Elements();
             foreach (var item in xlsa)
             {
                 AudioPresetComboBox.Items.Add(item.Attribute("Name").Value);
@@ -1274,7 +1204,6 @@ namespace mp4box
                 HelpTextBox.LoadFile(startpath + "\\help.rtf");
             }
 
-            PresetXml();
             LoadSettings();
         }
 
@@ -2273,7 +2202,7 @@ namespace mp4box
             if (x264ExeComboBox.Items != null)
             {
                 string encType = x264ExeComboBox.SelectedItem.ToString().Contains("x265") ? "x265" : "x264";
-                XElement xel = xdoc.Element("root").Element("Video").Element("VideoEncoder").Element(encType).Elements()
+                XElement xel = preset.GetVideoPreset(encType).Elements()
                                   .Where(x => x.Attribute("Name").Value == VideoPresetComboBox.Text).First();
                 x264CustomParameterTextBox.Text = xel.Value;
             }
@@ -2669,7 +2598,7 @@ namespace mp4box
                 if (!string.IsNullOrEmpty(vPresetName))
                 {
                     string encType = x264ExeComboBox.SelectedItem.ToString().Contains("x265") ? "x265" : "x264";
-                    var xl = xdoc.Element("root").Element("Video").Element("VideoEncoder").Element(encType);
+                    var xl = preset.GetVideoPreset(encType);
                     XElement xelnew = new XElement("Parameter", x264CustomParameterTextBox.Text,
                                           new XAttribute("Name", vPresetName));
                     foreach (var item in xl.Elements())
@@ -2681,7 +2610,7 @@ namespace mp4box
                         }
                     }
                     xl.Add(xelnew);
-                    xdoc.Save("preset.xml");
+                    preset.Save();
                     LoadVideoPreset();
                     VideoPresetComboBox.SelectedIndex = VideoPresetComboBox.FindString(vPresetName);
                 }
@@ -2699,7 +2628,7 @@ namespace mp4box
                 try
                 {
                     string encType = x264ExeComboBox.SelectedItem.ToString().Contains("x265") ? "x265" : "x264";
-                    var xls = xdoc.Element("root").Element("Video").Element("VideoEncoder").Element(encType).Elements();
+                    var xls = preset.GetVideoPreset(encType).Elements();
                     foreach (var item in xls)
                     {
                         if (item.Attribute("Name").Value == VideoPresetComboBox.Text)
@@ -2708,7 +2637,7 @@ namespace mp4box
                             break;
                         }
                     }
-                    xdoc.Save("preset.xml");
+                    preset.Save();
                     LoadVideoPreset();
                 }
                 catch (Exception ex)
@@ -3017,7 +2946,7 @@ namespace mp4box
 
             AudioPresetComboBox.Items.Clear();
             AudioCustomParameterTextBox.Text = string.Empty;
-            XElement xAudios = xdoc.Element("root").Element("Audio").Element("AudioEncoder").Element(AudioEncoderComboBox.Text);
+            var xAudios = preset.GetAudioPreset(AudioEncoderComboBox.Text);
             if (xAudios != null)
             {
                 foreach (XElement item in xAudios.Elements())
@@ -4512,7 +4441,7 @@ namespace mp4box
 
         private void AudioPresetComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            XElement x = xdoc.Element("root").Element("Audio").Element("AudioEncoder").Element(AudioEncoderComboBox.Text).Elements()
+            XElement x = preset.GetAudioPreset(AudioEncoderComboBox.Text).Elements()
                              .Where(_ => _.Attribute("Name").Value == AudioPresetComboBox.Text).First();
             AudioCustomParameterTextBox.Text = x.Value;
         }
@@ -4576,7 +4505,7 @@ namespace mp4box
 
                 VideoPresetComboBox.Items.Clear();
                 x264CustomParameterTextBox.Text = string.Empty;
-                XElement xVideos = xdoc.Element("root").Element("Video").Element("VideoEncoder").Element("x265");
+                var xVideos = preset.GetVideoPreset("x265");
                 if (xVideos != null)
                 {
                     foreach (XElement item in xVideos.Elements())
@@ -4600,7 +4529,7 @@ namespace mp4box
 
                 VideoPresetComboBox.Items.Clear();
                 x264CustomParameterTextBox.Text = string.Empty;
-                XElement xVideos = xdoc.Element("root").Element("Video").Element("VideoEncoder").Element("x264");
+                var xVideos = preset.GetVideoPreset("x265");
                 if (xVideos != null)
                 {
                     foreach (XElement item in xVideos.Elements())
@@ -4618,7 +4547,7 @@ namespace mp4box
             {
                 try
                 {
-                    var xls = xdoc.Element("root").Element("Audio").Element("AudioEncoder").Element(AudioEncoderComboBox.Text).Elements();
+                    var xls = preset.GetAudioPreset(AudioEncoderComboBox.Text).Elements();
                     foreach (var item in xls)
                     {
                         if (item.Attribute("Name").Value == AudioPresetComboBox.Text)
@@ -4627,7 +4556,7 @@ namespace mp4box
                             break;
                         }
                     }
-                    xdoc.Save("preset.xml");
+                    preset.Save();
                     LoadAudioPreset();
                 }
                 catch (Exception ex)
@@ -4644,7 +4573,7 @@ namespace mp4box
                 string aPresetName = InputBox.Show("请输入这个预设名称", "请为预置配置命名", "新预置名称");
                 if (!string.IsNullOrEmpty(aPresetName))
                 {
-                    var xl = xdoc.Element("root").Element("Audio").Element("AudioEncoder").Element(AudioEncoderComboBox.Text);
+                    var xl = preset.GetAudioPreset(AudioEncoderComboBox.Text);
                     XElement xelnew = new XElement("Parameter", AudioCustomParameterTextBox.Text,
                                           new XAttribute("Name", aPresetName));
                     foreach (var item in xl.Elements())
@@ -4656,7 +4585,7 @@ namespace mp4box
                         }
                     }
                     xl.Add(xelnew);
-                    xdoc.Save("preset.xml");
+                    preset.Save();
                     LoadAudioPreset();
                     AudioPresetComboBox.SelectedIndex = AudioPresetComboBox.FindString(aPresetName);
                 }
---
+++ mp4box/Preset.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace mp4box
+{
+    public class Preset
+    {
+        const string XMLFileName = "preset.xml";
+        XDocument xdoc = new XDocument();
+
+        public Preset()
+        {
+            Load();
+        }
+
+        public void Load()
+        {
+            if (!File.Exists(XMLFileName))
+                CreateDefaultAndSave();
+            else
+                xdoc = XDocument.Load(XMLFileName);
+        }
+
+        public void Save()
+        {
+            xdoc.Save(XMLFileName);
+        }
+
+        #region Create Default
+
+        public void CreateDefaultAndSave()
+        {
+            CreateDefault();
+            Save();
+        }
+
+        public void CreateDefault()
+        {
+            xdoc = new XDocument(
+                    new XDeclaration("1.0", "utf-8", "yes"),
+                    new XElement("root",
+                        new XElement("Video",
+                            new XElement("VideoEncoder",
+                                new XElement("x264",
+                                    new XElement("Parameter", "--crf 24 --preset 8 -r 6 -b 6 -i 1 --scenecut 60 -f 1:1 --qcomp 0.5 --psy-rd 0.3:0 --aq-mode 2 --aq-strength 0.8 --vf resize:960,540,,,,lanczos", new XAttribute("Name", "default")),
+                                    new XElement("Parameter", "-p1 --crf 20 --aq-mode 2 --sar 32:27 --vf yadif:, --stat \"TEMP.stat\" --slow-firstpass --ref 8 --preset 8 --subme 10", new XAttribute("Name", "DVDRIP不切边,16:9")),
+                                    new XElement("Parameter", "-p1 --crf 20 --aq-mode 2 --sar 40:33 --vf yadif:, --stat \"TEMP.stat\" --slow-firstpass --ref 8 --preset 8 --subme 10", new XAttribute("Name", "DVDRIP不切边,sar40:33")),
+                                    new XElement("Parameter", "-p1 --crf 20 --aq-mode 2 --sar 40:33 --vf yadif:,/crop:8,0,8,0 --stat \"TEMP.stat\" --slow-firstpass --ref 8 --preset 8 --subme 10", new XAttribute("Name", "DVDRIP切边,sar40:33")),
+                                    new XElement("Parameter", "--profile high --level 3.1 --level-force  --device iphone", new XAttribute("Name", "iOS")),
+                                    new XElement("Parameter", "--crf 25 --preset placebo --subme 10 --ref 7 --bframes 7 --qcomp 0.75 --psy-rd 0:0 --keyint infinite --min-keyint 1", new XAttribute("Name", "MAD")),
+                                    new XElement("Parameter", "--profile main --level 3.0 --ref 3 --b-pyramid none --weightp 1 --vbv-maxrate 10000 --vbv-bufsize 10000   --vf resize:480,272,,,,lanczos  --device psp", new XAttribute("Name", "PSP"))
+                                    ),
+                                new XElement("x265",
+                                    new XElement("Parameter", "", new XAttribute("Name", "x265default"))
+                                    )
+                                )
+                            ),
+                        new XElement("Audio",
+                            new XElement("AudioEncoder",
+                                new XElement("NeroAAC",
+                                    new XElement("Parameter", "-he -br 32000", new XAttribute("Name", "NeroAAC_HE-32Kbps")),
+                                    new XElement("Parameter", "-he -br 48000", new XAttribute("Name", "NeroAAC_HE-48Kbps")),
+                                    new XElement("Parameter", "-he -br 64000", new XAttribute("Name", "NeroAAC_HE-64Kbps")),
+                                    new XElement("Parameter", "-lc -br 128000", new XAttribute("Name", "NeroAAC_LC-128Kbps")),
+                                    new XElement("Parameter", "-lc -br 192000", new XAttribute("Name", "NeroAAC_LC-192Kbps")),
+                                    new XElement("Parameter", "-lc -br 256000", new XAttribute("Name", "NeroAAC_LC-256Kbps")),
+                                    new XElement("Parameter", "-q 1 -lc", new XAttribute("Name", "NeroAAC_LC-Q1"))
+                                    ),
+                                new XElement("FDKAAC",
+                                     new XElement("Parameter", "-m 1", new XAttribute("Name", "FDKAAC_VBR1")),
+                                     new XElement("Parameter", "-m 2", new XAttribute("Name", "FDKAAC_VBR2")),
+                                     new XElement("Parameter", "-m 3", new XAttribute("Name", "FDKAAC_VBR3")),
+                                     new XElement("Parameter", "-m 4", new XAttribute("Name", "FDKAAC_VBR4")),
+                                     new XElement("Parameter", "-m 5", new XAttribute("Name", "FDKAAC_VBR5")),
+                                     new XElement("Parameter", "-p 5 -b 64", new XAttribute("Name", "FDKAAC_HE-_CBR64Kbps")),
+                                     new XElement("Parameter", "-b 128", new XAttribute("Name", "FDKAAC_LC-_CBR128Kbps")),
+                                     new XElement("Parameter", "-b 192", new XAttribute("Name", "FDKAAC_LC-_CBR192Kbps")),
+                                     new XElement("Parameter", "-b 256", new XAttribute("Name", "FDKAAC_LC-_CBR256Kbps"))
+                                     ),
+                                new XElement("QAAC",
+                                    new XElement("Parameter", "--he -c 64 -q 2 --no-optimize", new XAttribute("Name", "QAAC_HE-CBR64Kbps")),
+                                    new XElement("Parameter", "--he -v 64 -q 2 --no-optimize", new XAttribute("Name", "QAAC_HE-CVBR64Kbps")),
+                                    new XElement("Parameter", "-c 128 -q 2 --no-optimize", new XAttribute("Name", "QAAC_LC-CBR128Kbps")),
+                                    new XElement("Parameter", "-v 128 -q 2 --no-optimize", new XAttribute("Name", "QAAC_LC-CVBR128Kbps")),
+                                    new XElement("Parameter", "-c 256 -q 2 --no-optimize", new XAttribute("Name", "QAAC_LC-CBR256Kbps")),
+                                    new XElement("Parameter", "-v 256 -q 2 --no-optimize", new XAttribute("Name", "QAAC_LC-CVBR256Kbps")),
+                                    new XElement("Parameter", "-V 90 -q 2 --no-optimize", new XAttribute("Name", "QAAC_TVBR_V90")),
+                                    new XElement("Parameter", "-V 127 -q 2 --no-optimize", new XAttribute("Name", "QAAC_TVBR_V127"))
+                                    ),
+                                new XElement("MP3",
+                                    new XElement("Parameter", "--alt-preset extreme", new XAttribute("Name", "extreme")),
+                                    new XElement("Parameter", "--preset insane", new XAttribute("Name", "insane")),
+                                    new XElement("Parameter", "--preset standard", new XAttribute("Name", "standard")),
+                                    new XElement("Parameter", "--preset medium ", new XAttribute("Name", "medium"))
+                                    )
+                                )
+                            )
+                        )
+                );
+        }
+
+        #endregion
+
+
+        public XElement GetVideoPreset(string encType)
+        {
+            return xdoc.Element("root").Element("Video").Element("VideoEncoder").Element(encType);
+        }
+
+        public XElement GetAudioPreset(string encType)
+        {
+            return xdoc.Element("root").Element("Audio").Element("AudioEncoder").Element(encType);
+        }
+
+    }
+}
