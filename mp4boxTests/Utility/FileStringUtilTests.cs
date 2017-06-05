273babb3f0e58f23b49046c52ea727e30f9066f2
celeron533 <zhuyilun8@hotmail.com>
2017-06-05 22:24:29 +0800

    Create tests for SpeculateSubtitlePath()

---
+++ mp4boxTests/Utility/FileStringUtilTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using mp4box.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mp4box.Utility.Tests
+{
+    [TestClass()]
+    public class FileStringUtilTests
+    {
+        [TestMethod()]
+        public void SpeculateSubtitlePathTest()
+        {
+            // subtitle file does not exist
+            string videoFileName = "dummyVideo.mp5";
+            var result = FileStringUtil.SpeculateSubtitlePath(videoFileName);
+            Assert.AreEqual(result, string.Empty);
+        }
+
+        [TestMethod()]
+        public void SpeculateSubtitlePathTest2()
+        {
+            string videoFileName = "dummyVideo.mp5";
+            string subtitleFileName = "dummyVideo.sub";
+            System.IO.File.Create(subtitleFileName).Dispose();
+
+            var result = FileStringUtil.SpeculateSubtitlePath(videoFileName);
+            Assert.AreEqual(result, string.Empty);
+
+            System.IO.File.Delete(subtitleFileName);
+        }
+
+        [TestMethod()]
+        public void SpeculateSubtitlePathTest3()
+        {
+            // with language
+            string videoFileName = "dummyVideo.mp5";
+            string subtitleFileName = "dummyVideo.eng.sub";
+            System.IO.File.Create(subtitleFileName).Dispose();
+
+            var result = FileStringUtil.SpeculateSubtitlePath(videoFileName,"eng");
+            Assert.AreEqual(result, string.Empty);
+
+            System.IO.File.Delete(subtitleFileName);
+        }
+    }
+}
