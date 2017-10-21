ab8e8611812845aa820dd7e090782673cd2e0762
celeron533 <zhuyilun8@hotmail.com>
2017-10-21 11:21:39 +0800

    Minor change

---
+++ mp4box/Enums.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mp4box
+{
+    public enum MediaType
+    {
+        Audio,
+        Video
+    }
+
+    public enum X264Mode
+    {
+        Custom,
+        Crf,
+        TwoPass
+    }
+
+    public enum AudioEncoder
+    {
+        // order sensitive
+        NA = -1,
+        NeroAAC = 0,
+        QAAC,
+        WAV,
+        ALAC,
+        FLAC,
+        FDKAAC,
+        AC3,
+        MP3
+    }
+
+    public enum AudioMode
+    {
+        Custom,
+        Bitrate
+    }
+
+    public enum VideoEncoder
+    {
+        NA = -1,
+        X264 = 0,
+        X265
+    }
+}
