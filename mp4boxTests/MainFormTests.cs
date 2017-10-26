ca19bb54799cff783718e3681e2f5005e0de0e7a
celeron533 <zhuyilun8@hotmail.com>
2017-10-26 21:55:14 +0800

    Fix broken test

--- mp4boxTests/MainFormTests.cs
+++ mp4boxTests/MainFormTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using mp4box.Procedure;
 using mp4box.Utility;
 using System;
 using System.Collections.Generic;
@@ -13,25 +14,22 @@ namespace mp4box.Tests
         [TestMethod()]
         public void ffmuxbatTest()
         {
-            MainForm mf = new MainForm();
             string input1 = "input1";
             string input2 = "input2";
             string output = "output";
             string resultBeforeRefactor = "\"" + ToolsUtil.FFMPEG.fullPath + "\" -i \"" + input1 + "\" -i \"" + input2 + "\" -sn -c copy -y \"" + output + "\"";
-            string result = mf.FFMpegMuxCommand(input1, input2, output);
+            string result = Shared.FFMpegMuxCommand(input1, input2, output);
             Assert.AreEqual(resultBeforeRefactor, result);
         }
 
         [TestMethod()]
         public void boxmuxbatTest()
         {
-            MainForm mf = new MainForm();
-
             string input1 = "input1";
             string input2 = "input2";
             string output = "output";
             string resultBeforeRefactor = "\"" + ToolsUtil.MP4BOX.fullPath + "\" -add \"" + input1 + "#trackID=1:name=\" -add \"" + input2 + "#trackID=1:name=\" -new \"" + output + "\"";
-            string result = mf.MP4MuxCommand(input1, input2, output);
+            string result = Shared.MP4MuxCommand(input1, input2, output);
             Assert.AreEqual(resultBeforeRefactor, result);
         }
 
